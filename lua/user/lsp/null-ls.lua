5750a586a0721e831a58fc7c84b9b31f615d94cb
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2022-11-15 22:00:29 +0200

    Learned to 'stylua ignore'

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,47 +9,45 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
-null_ls.setup {
-  debug = false,
-   sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml" },
-      extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' },
-      filetypes = {
-        'javascript',
-        'javascriptreact',
-        'typescript',
-        'typescriptreact',
-        'vue',
-        'css',
-        'scss',
-        'less',
-        'html',
-        'json',
-        'jsonc',
-        'yaml',
-        'graphql',
-        'handlebars',
-      },
-    },
-    formatting.black.with { extra_args = { '--fast' } },
-    formatting.stylua,
-    formatting.rustfmt,
-    formatting.google_java_format,
-    diagnostics.flake8,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method 'textDocument/formatting' then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format { bufnr = bufnr }
-        end,
-      })
-    end
-  end
-}
-
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_filetypes = { "toml" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"vue",
+				"css",
+				"scss",
+				"less",
+				"html",
+				"json",
+				"jsonc",
+				"yaml",
+				"graphql",
+				"handlebars",
+			},
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		formatting.rustfmt,
+		formatting.google_java_format,
+		diagnostics.flake8,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
